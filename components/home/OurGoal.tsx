2e6a259f17474fbf53921c04081b6ea15405cffa
e.nicholas987 <i.emeka74@yahoo.com>
2024-08-19 11:53:07 +0100

    feat: updates

--- components/home/OurGoal.tsx
+++ components/home/OurGoal.tsx
@@ -4,12 +4,7 @@ import { FiArrowRight } from "react-icons/fi";
 
 export default function OurGoal() {
   return (
-    <div
-      style={{
-        background: "linear-gradient(90deg, #00A03B 15.5%, #AFCB37 100%)",
-      }}
-      className="py-[5.125rem] xl:py-[3.125rem] text-white px-10"
-    >
+    <div className="py-[5.125rem] xl:py-[3.125rem] text-white px-10 bg-[#3C7E79]">
       <p className="text-2xl leading-9 max-w-[75rem] mx-auto text-center text-pretty">
         Our goal at Teva is to ensure that people from all backgrounds and walks
         of life have access to our research studies.
--- components/home/ReasonsForLackOfDiversity.tsx
+++ components/home/ReasonsForLackOfDiversity.tsx
@@ -1,35 +1,65 @@
-import SectionPicture from "../SectionPicture";
-import ManAndTwoKids from "/public/assets/man-and-two-kids.jpeg";
+import Settings from "/public/assets/settings.svg";
+import HeartBreadk from "/public/assets/heart-break.svg";
+import Question from "/public/assets/question.svg";
+import Inaccessibility from "/public/assets/inaccessibility.svg";
+import Image from "next/image";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
-const reasons: string[] = [
-  "Medical mistrust due to historical events and patients’ personal experiences with the healthcare system.",
-  "Unconscious bias because of ingrained beliefs about who may be interested or capable of participating.",
-  "Lack of knowledge/awareness because patients simply don’t know these options exist, and medical professionals may not have time to stay up-to-date on available studies.",
-  "Inaccessibility due to factors that make participation difficult (eg, lack of language or disability accommodations, and time/expense for getting to the study site).",
+interface Reason {
+  icon: string | StaticImport;
+  description: string;
+}
+
+const reasons: Reason[] = [
+  {
+    icon: Settings,
+    description:
+      "Unconscious bias because of ingrained beliefs about who may be interested or capable of participating.",
+  },
+  {
+    icon: HeartBreadk,
+    description:
+      "Medical mistrust due to historical events and patients’ personal experiences with the healthcare system.",
+  },
+  {
+    icon: Question,
+    description:
+      "Lack of knowledge/awareness because patients simply don’t know these options exist, and medical professionals may not have time to stay up-to-date on available studies.",
+  },
+  {
+    icon: Inaccessibility,
+    description:
+      "Inaccessibility due to factors that make participation difficult (eg, lack of language or disability accommodations, and time/expense for getting to the study site).",
+  },
 ];
 
 export default function ReasonsForLackOfDiversity() {
   return (
     <section className="py-[3.75rem] xl:py-[6.5rem] flex flex-col items-center xl:flex-row justify-center px-6 xl:px-[6.625rem] gap-28 xl:gap-[7.625rem] xl:items-start text-body-text">
-      <div className="max-w-[43.125rem] w-full">
-        <h2 className="heading leading-9 text-pretty">
-          Historically, there was inequality in medical research, leading to
-          underrepresentation of some communities.
+      <div className="max-w-[75.125rem] w-full flex flex-col items-center">
+        <h2 className="heading leading-9 text-center">
+          There has been a long history of inequality in medical research,
+          leading to underrepresentation of some communities.
         </h2>
-        <p className="mt-6">Some reasons for the lack of diversity included:</p>
-        <ul className="ml-5 mt-10 list-disc">
+        <p className="mt-6 text-center ">
+          Some reasons for the lack of diversity included:
+        </p>
+        <ul className="ml-9 mt-10 xl:grid-cols-4 grid md:grid-cols-2 gap-10 max-w-[32.125rem] xl:max-w-full">
           {reasons.map((reason) => (
-            <li key={reason} className="leading-7 max-w-[32.125rem]">
-              {reason}
+            <li
+              key={reason.description}
+              className="flex-col gap-5 fap-leading-7 flex justify-center items-center"
+            >
+              <Image src={reason.icon} alt={reason.description} />
+              <p className="text-center">{reason.description}</p>
             </li>
           ))}
         </ul>
-        <p className="mt-10 max-w-[32.125rem]">
+        <p className="mt-10 text-center">
           By recognizing these factors, we can now do better to ensure everyone
           has access to research studies.
         </p>
       </div>
-      <SectionPicture src={ManAndTwoKids} alt="Man and two kids" />
     </section>
   );
 }
